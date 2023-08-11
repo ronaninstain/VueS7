0b4db6f6be4c87c29a48ee6aae49db00a9fca166
ronaninstain <shoivehossain100@gmail.com>
2023-08-11 19:17:23 +0600

    Componets are time savers

---
+++ vue-components-4/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ParentComponent from "./components/ParentComponent.vue";
+</script>
+
+<template>
+  <section class="mx-auto ">
+    <ParentComponent />
+  </section>
+</template>
+
+<style scoped></style>
